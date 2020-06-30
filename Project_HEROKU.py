e2eb5b16b96971e6136d363f9d4f1cde28db05bf
Oorgien <xmaximuskn@gmail.com>
2020-06-30 21:21:17 +0300

    128->256

--- Project_HEROKU.py
+++ Project_HEROKU.py
@@ -111,11 +111,11 @@ def get_style_2(message):
 def train_NST(message, mode):
     bot.send_message(message.chat.id, "Подождите немного! Алгоритм медленный:(")
     if mode == 1:
-        style1_img = Preproc(128).image_loader('images/style.jpg')
-        content_img = Preproc(128).image_loader('images/content.jpg')
+        style1_img = Preproc(256).image_loader('images/style.jpg')
+        content_img = Preproc(256).image_loader('images/content.jpg')
         input_img = content_img.clone()
         output = Style_transfer(content_img=content_img, style1_img=style1_img,
-                                style2_img=torch.zeros(1, 3, 128, 128), input_img=input_img,
+                                style2_img=torch.zeros(1, 3, 256, 256), input_img=input_img,
                                 num_steps=100, style_weight_1=100000, style_weight_2=100000, mode=1).forward()
     else:
         style1_img, style2_img, content_img = Preproc(128).transform_images("images/content.jpg",
@@ -142,7 +142,7 @@ def run_GAN(message, name):
     elif name == 'vangogh':
         model = 'style_vangogh_pretrained'
 
-    res = GanModel(model).forward('images/content.jpg', 255)
+    res = GanModel(model).forward('images/content.jpg', 256)
 
     torchvision.utils.save_image(res, 'images/output.jpg')
     photo = open('images/output.jpg', 'rb')
